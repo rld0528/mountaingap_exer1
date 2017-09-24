33e4059778b2f5ce8550049e63ae1cee3149eeba
rld0528 <russellgilbertdecastro@gmail.com>
2017-09-24 10:41:22 +0800

    MBTA partial update

--- components/mbta/mbta.component.js
+++ components/mbta/mbta.component.js
@@ -6,7 +6,8 @@ angular.module('mbta').
 		controller: function ProjectListController($http){
 		   var self = this;
 
-		    $http.get('http://realtime.mbta.com/developer/api/v2/routesbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=place-bbsta&format=json').then(function(response) {
+		    $http.get('http://realtime.mbta.com/developer/api/v2/routesbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=place-bbsta&format=json').
+		    then(function(response) {
        			 self.mbtaRes = response.data;
     	  	});
 		    
